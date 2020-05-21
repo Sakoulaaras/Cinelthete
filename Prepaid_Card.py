ce08a2583dee80377dfdac28a7314bbb5cddab34
Petros11888 <petros11888@gmail.com>
2020-05-22 00:34:36 +0300

    Update Prepaid_Card.py

--- Prepaid_Card.py
+++ Prepaid_Card.py
@@ -1,14 +1,24 @@
 class Prepaid_Card:
-    def __init__(self):
-        pass
+    def __init__(self,number,balance):
+        self.number =number
+        self.balance = balance
     
     def getBalance(self):
         return self.balance 
+    def setBalance(self,balance):
+        self.balance = balance
     
-    def checkBalance(self):
-        pass
+    def checkBalance(self,price):
+        bal_and_miss_amount = {}
+        if self.getBalance()>price:
+            return True
+        else:
+            bal_and_miss_amount['balance']=self.getBalance()
+            bal_and_miss_amount['missing'] = price - self.getBalance()
+            return bal_and_miss_amount
 
-    def makeTransaction(self):
-        pass
+    def makeTransaction(self,price):
+        remainder = self.getBalance()-price
+        self.setBalance(remainder)
  
 
