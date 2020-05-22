bd13fcb626fb0d82015b5644acd27d98915dd107
antonisgrko <65362522+antonisgrko@users.noreply.github.com>
2020-05-22 20:10:13 +0300

    Update Ratings.py

--- Ratings.py
+++ Ratings.py
@@ -2,8 +2,14 @@ class Ratings:
     def __init__(self):
         pass
     
-    def validateRatingAndCritique(self):
-        pass
+    def validateRatingAndCritique(self,rating,critique):
+        if rating<0 or rating >10:
+            return False
+        else:
+            if len(critique)==0 or (len(critique)>5 and len(critique)<700):
+                return True
+            else:
+                return False
 
     def registerRating(self):
         pass
