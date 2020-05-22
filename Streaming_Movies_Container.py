ff46de36b4a562a386fabe358b7918e96313a5ce
vaggelis96 <65599759+vaggelis96@users.noreply.github.com>
2020-05-22 13:25:09 +0300

    Update Streaming_Movies_Container.py

--- Streaming_Movies_Container.py
+++ Streaming_Movies_Container.py
@@ -1,9 +1,25 @@
 class Streaming_Movies_Container:
-    def __init__(self):
-        pass
+    def __init__(self,live_streams,upcoming_streams):
+        self.live_streams = live_streams
+        self.upcoming_streams = upcoming_streams
+        
+    def addLiveStream(self,stream):
+        self.live_streams.append(stream)
+
+    def addUpcomingStream(self,stream):
+        self.upcoming_streams.append(stream)
+
+    def getLiveStreams(self):
+        return self.live_streams
+    
+    def getUpcomingStreams(self):
+        return self.upcoming_streams
 
     def checkLiveStreams(self):
-        pass
+         if not self.getLiveStreams():
+            return False
+        else:
+            return self.getLiveStreams()
 
     def retrieveUpcomingStreams(self):
         pass
