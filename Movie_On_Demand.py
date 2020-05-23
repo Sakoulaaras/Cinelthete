d0dd7887908b0fdf368dca700af152220b3348d3
vaggelis96 <65599759+vaggelis96@users.noreply.github.com>
2020-05-23 13:33:46 +0300

    Update Movie_On_Demand.py

--- Movie_On_Demand.py
+++ Movie_On_Demand.py
@@ -1,3 +1,14 @@
 class Movie_On_Demand:
-    def __init__(self):
-        pass
+    def __init__(self,director,genre,starring):
+        self.director = director
+        self.genre = genre
+        self.starring = starring
+
+    def getDirector(self):
+        return self.director
+    
+    def getGenre(self):
+        return self.genre
+
+    def getStarring(self):
+        return self.starring
