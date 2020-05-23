3d30901c5afbe0b9653be294af1205451d80c037
vaggelis96 <65599759+vaggelis96@users.noreply.github.com>
2020-05-23 13:32:04 +0300

    Update Movie_On_Demand_Service.py

--- Movie_On_Demand_Service.py
+++ Movie_On_Demand_Service.py
@@ -1,4 +1,17 @@
 class Movie_On_Demand_Service:
-    def __init__(self):
-        pass
+    def __init__(self,activated):
+        self.activated = activated
+
+    def checkMovieOnDemandService(self):
+        if self.getActivated() == True:
+            return True
+        else:
+            return False
+
+    def getActivated(self):
+        return self.activated
+    
+    # gia prosorini apenergoiisi tis ipiresias 
+    def setActivated(self,activated):
+        self.activated = activated
 
