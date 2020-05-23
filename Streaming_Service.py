2aabb269f0d53c8a704946a4c4b9908a1b82b9b6
vaggelis96 <65599759+vaggelis96@users.noreply.github.com>
2020-05-23 13:33:04 +0300

    Update Streaming_Service.py

--- Streaming_Service.py
+++ Streaming_Service.py
@@ -3,9 +3,16 @@ class Streaming_Service:
         self.activated = activated
 
     def checkStreamingService(self):
-        if self.activated == True:
+        if self.getAtivated()  True:
             return True
         else:
             return False
+        
+    def getActivated(self):
+        return self.activated
+
+    # gia prosorini apenergopoiisi tis ipiresias
+    def setActivated(self,activated):
+        self.activated = activated
 
 
