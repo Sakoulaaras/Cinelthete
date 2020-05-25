f3b08829c3531b8ddda0a611b81ed855f9869925
Sakoulaaras <64432119+Sakoulaaras@users.noreply.github.com>
2020-05-25 20:37:39 +0300

    Update Ratings.py

--- Ratings.py
+++ Ratings.py
@@ -1,6 +1,10 @@
+from Client import Client
+from Streaming_Movie import Streaming_Movie
+from Classic_Movie import Classic_Movie
+from Mainstream_Movie import Mainstream_Movie
 class Ratings:
     def __init__(self):
-        pass
+        self.ratings_log = {}
     
     def validateRatingAndCritique(self,rating,critique):
         if rating<0 or rating >10:
@@ -15,7 +19,18 @@ class Ratings:
         pass
     
     def checkRatedMovies(self):
-        pass
+        total_ratings = len(self.ratings_log[client.getId()]['Streaming_Movies'])+len(self.ratings_log[client.getId()]['Classic_Movies'])+len(self.ratings_log[client.getId()]['Mainstream_Movies'])
+        if total_ratings>=10:
+            return True
+        else:
+            return False
 
     def checkRatedClassicMovies(self):
-        pass
+        total_ratings = len(self.ratings_log[client.getId()]['Streaming_Movies'])+len(self.ratings_log[client.getId()]['Classic_Movies'])+len(self.ratings_log[client.getId()]['Mainstream_Movies'])
+        if len(self.ratings_log[client.getId()]['Classic_Movies'])>=5:
+            return True
+        else:
+            if total_ratings>=5:
+                return 'He belongs to a genre cluster'
+            else:
+                return False
