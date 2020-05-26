ad71cc0e5b629a244c090e7549355a18a7e9046f
vaggelis96 <65599759+vaggelis96@users.noreply.github.com>
2020-05-26 21:26:27 +0300

    Update Streaming_Movie.py

--- Streaming_Movie.py
+++ Streaming_Movie.py
@@ -1,11 +1,18 @@
 class Streaming_Movie:
-    
-    def __init__(self,director,genre,starring):
+    class_counter = 0
+    def __init__(self,title,director,genre,starring):
+        self.title = title
         self.director = director
         self.genre = genre
         self.starring = starring
+        self.id = Streaming_Movie.class_counter
+        Streaming_Movie.class_counter += 1
         self.people_watching_now = 0
         self.max_people_watching = 0
+        self.mean_rating = 0
+        
+    def getTitle(self):
+        return self.title
         
     def updatePeopleWatchingNow(self):
         self.people_watching_now += 1
