d7472f2219c0858b966fe236a8946d356e2af27a
vaggelis96 <65599759+vaggelis96@users.noreply.github.com>
2020-05-26 21:29:12 +0300

    Update Mainstream_Movie.py

--- Mainstream_Movie.py
+++ Mainstream_Movie.py
@@ -1,7 +1,7 @@
 from Movie_On_Demand import Movie_On_Demand
 class Mainstream_Movie(Movie_On_Demand):
-    def __init__(self,director,genre,starring,year):
-        super(Mainstream_Movie,self).__init__(director,genre,starring)
+    def __init__(self,title,director,genre,starring,year):
+        super(Mainstream_Movie,self).__init__(title,director,genre,starring)
         self.year = year
 
     def getYear(self):
