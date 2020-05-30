1b5e07bce01b52302b047408934d646bc1fdef54
vaggelis96 <65599759+vaggelis96@users.noreply.github.com>
2020-05-30 20:49:16 +0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -17,6 +17,59 @@ def get_genre_ratings(ratings, movies, genres, column_names):
     genre_ratings.columns = column_names
     return genre_ratings
  
+    
+def draw_movie_clusters(clustered, max_users, max_movies):
+    c=1
+    for cluster_id in clustered.group.unique():
+        # gia na einai pio emfanes, deixnoume to poli max_users users kai max_movies movies ana cluster.
+        # Boroun na allaksoun autes i times gia na doume perisoterous users kai tainies ana cluster
+        d = clustered[clustered.group == cluster_id].drop(['index', 'group'], axis=1)
+        n_users_in_cluster = d.shape[0]
+        
+        d = sort_by_rating_density(d, max_movies, max_users)
+        
+        d = d.reindex_axis(d.mean().sort_values(ascending=False).index, axis=1)
+        d = d.reindex_axis(d.count(axis=1).sort_values(ascending=False).index)
+        d = d.iloc[:max_users, :max_movies]
+        n_users_in_plot = d.shape[0]
+        
+        # mono ta  clusters me perisoterous apo 9 users, alliws den ma endiaferoun
+        if len(d) > 9:
+            print('cluster # {}'.format(cluster_id))
+            print('# of users in cluster: {}.'.format(n_users_in_cluster), '# of users in plot: {}'.format(n_users_in_plot))
+            fig = plt.figure(figsize=(15,4))
+            ax = plt.gca()
+
+            ax.invert_yaxis()
+            ax.xaxis.tick_top()
+            labels = d.columns.str[:40]
+
+            ax.set_yticks(np.arange(d.shape[0]) , minor=False)
+            ax.set_xticks(np.arange(d.shape[1]) , minor=False)
+
+            ax.set_xticklabels(labels, minor=False)
+                        
+            ax.get_yaxis().set_visible(False)
+
+            # Heatmap
+            heatmap = plt.imshow(d, vmin=0, vmax=5, aspect='auto')
+
+            ax.set_xlabel('movies')
+            ax.set_ylabel('User id')
+
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="5%", pad=0.05)
+
+            # Color bar
+            cbar = fig.colorbar(heatmap, ticks=[5, 4, 3, 2, 1, 0], cax=cax)
+            cbar.ax.set_yticklabels(['5 stars', '4 stars','3 stars','2 stars','1 stars','0 stars'])
+
+            plt.setp(ax.get_xticklabels(), rotation=90, fontsize=9)
+            plt.tick_params(axis='both', which='both', bottom='off', top='off', left='off', labelbottom='off', labelleft='off') 
+            #print('cluster # {} \n(Showing at most {} users and {} movies)'.format(cluster_id, max_users, max_movies))
+
+            plt.show()
+    
                 
 def get_most_rated_movies(user_movie_ratings, max_number_of_movies):
 
@@ -28,3 +81,60 @@ def get_most_rated_movies(user_movie_ratings, max_number_of_movies):
     most_rated_movies = user_movie_ratings_sorted.iloc[:, :max_number_of_movies]
     return most_rated_movies
 
+
+def get_users_who_rate_the_most(most_rated_movies, max_number_of_movies):
+    # Oi users me tis perisoteres aksiologiseis
+    # 1- Count
+    most_rated_movies['counts'] = pd.Series(most_rated_movies.count(axis=1))
+    # 2- Sort
+    most_rated_movies_users = most_rated_movies.sort_values('counts', ascending=False)
+    # 3- Slice
+    most_rated_movies_users_selection = most_rated_movies_users.iloc[:max_number_of_movies, :]
+    most_rated_movies_users_selection = most_rated_movies_users_selection.drop(['counts'], axis=1)
+    
+    return most_rated_movies_users_selection
+
+def sort_by_rating_density(user_movie_ratings, n_movies, n_users):
+    most_rated_movies = get_most_rated_movies(user_movie_ratings, n_movies)
+    most_rated_movies = get_users_who_rate_the_most(most_rated_movies, n_users)
+    return most_rated_movies
+
+
+def draw_movies_heatmap(most_rated_movies_users_selection, axis_labels=True):
+    
+    # Reverse gia na tairiazei me to trexon dataframe
+    #most_rated_movies_users_selection = most_rated_movies_users_selection.iloc[::-1]
+    
+    fig = plt.figure(figsize=(15,4))
+    ax = plt.gca()
+    
+    # Draw heatmap
+    heatmap = ax.imshow(most_rated_movies_users_selection,  interpolation='nearest', vmin=0, vmax=5, aspect='auto')
+
+    if axis_labels:
+        ax.set_yticks(np.arange(most_rated_movies_users_selection.shape[0]) , minor=False)
+        ax.set_xticks(np.arange(most_rated_movies_users_selection.shape[1]) , minor=False)
+        ax.invert_yaxis()
+        ax.xaxis.tick_top()
+        labels = most_rated_movies_users_selection.columns.str[:40]
+        ax.set_xticklabels(labels, minor=False)
+        ax.set_yticklabels(most_rated_movies_users_selection.index, minor=False)
+        plt.setp(ax.get_xticklabels(), rotation=90)
+    else:
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+    
+    ax.grid(False)
+    ax.set_ylabel('User id')
+
+    # diaxwrizw heatmap apo to color bar
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+
+    # Color bar
+    cbar = fig.colorbar(heatmap, ticks=[5, 4, 3, 2, 1, 0], cax=cax)
+    cbar.ax.set_yticklabels(['5 stars', '4 stars','3 stars','2 stars','1 stars','0 stars'])
+
+
+
+    plt.show()
