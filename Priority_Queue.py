d6bf2af90d6cac1c950672387c409d1950584d71
antonisgrko <65362522+antonisgrko@users.noreply.github.com>
2020-05-31 17:02:33 +0300

    Update Priority_Queue.py

--- Priority_Queue.py
+++ Priority_Queue.py
@@ -5,6 +5,11 @@ class Priority_Queue:
     def insertToPriorityQueue(self,client):
         self.on_hold.append(client)
 
-    def calculateReservationTime(self):
-        pass
+    def calculateReservationTime(self,hours,minutes,seconds):
+        wra_tainias = timedelta(seconds=seconds,minutes=minutes,hours=hour)
+        trexousa_wra = timedelta(seconds=datetime.now().second,minutes=datetime.now().minute,hours=datetime.now().hour)
+        dinati_wra_kratisis = wra_tainias - timedelta(minutes=30)
+        apomenei = dinati_wra_kratisis - trexousa_wra
+        print(f'Dinati wra kratisis: {dinati_wra_kratisis}')
+        return apomenei
     
