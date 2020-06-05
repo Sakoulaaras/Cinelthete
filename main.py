3f0bf024a9a2ffd338792e2829206eb8cb1e9f83
vaggelis96 <65599759+vaggelis96@users.noreply.github.com>
2020-06-05 21:12:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from Admin import Admin
+from Cinema import Cinema
+from Classic_Movie import Classic_Movie
+from Client import Client
+from Company import Company
+from Credit_Card import Credit_Card
+from Mainstream_Movie import Mainstream_Movie
+from Movie_On_Demand_Service import Movie_On_Demand_Service
+from Movie_On_Demand import Movie_On_Demand
+from Movie import Movie
+from Omades import Omades
+from Prepaid_Card import Prepaid_Card
+from Priority_Queue import Priority_Queue
+from Ratings import Ratings
+from Recommended_Movie import Recommended_Movie
+from Screening_Hall import Screening_Hall
+from Seat import Seat
+from Streaming_Movie import Streaming_Movie
+from Streaming_Movies_Container import Streaming_Movies_Container
+from Streaming_Service import Streaming_Service
+from Ticket import Ticket
+from Trading_System import Trading_System
+
+
+
+
+
+client1 = Client('kostis','Petrakis','kpetrakis@ceid.upatras.gr',25)
+client2 = Client('Vaggelis','Petrakis','vpetrakis@ceid.upatras.gr',24)
+client3 = Client('Petros','Eleutheriadis','peleutheriadis@ceid.upatras.gr',24)
+client4 = Client('Antonis','Karvounis','akarvounis@ceid.upatras.gr',24)
+
+
+
+
